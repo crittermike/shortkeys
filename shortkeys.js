2f2b33372fc9167c97cbeebc69c7274437ecbc9d
Mike Crittenden <mikecrittenden@gmail.com>
2012-06-08 10:48:58 -0400

    add scrollto top and bottom

--- shortkeys.js
+++ shortkeys.js
@@ -1,2 +1,4 @@
 key('shift+h', function(){ history.back(); });
 key('shift+l', function(){ history.forward(); });
+key('g', function(){ window.scrollTo(0, 0); });
+key('shift+g', function(){ window.scrollTo(0, document.body.scrollHeight); });
