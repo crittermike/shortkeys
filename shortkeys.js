36315a6d3a48fb00f35722b3a7994b54957df047
Mike Crittenden <mikecrittenden@gmail.com>
2012-06-08 23:04:20 -0400

    add new actions! fix everying!

--- shortkeys.js
+++ shortkeys.js
@@ -1,4 +1,41 @@
-key('shift+h', function(){ history.back(); });
-key('shift+l', function(){ history.forward(); });
-key('g', function(){ window.scrollTo(0, 0); });
-key('shift+g', function(){ window.scrollTo(0, document.body.scrollHeight); });
+chrome.extension.sendRequest({method: "getKeys"}, function(response) {
+  var keys = response.keys;
+  if (keys) {
+    keys = JSON.parse(keys);
+    i = 0;
+    while (keys["key" + i] !== undefined) {
+      curkey = keys["key" + i];
+      key(curkey.key, find_action(curkey.action));
+      i++;
+    }
+  }
+
+  function find_action(shortname) {
+
+    if (shortname == "top") {
+      return function(){ window.scrollTo(0, 0); }
+    } else if (shortname == "bottom") {
+      return function(){ window.scrollTo(0, document.body.scrollHeight); };
+    } else if (shortname == "scrollup") {
+      return function(){ window.scrollBy(0,-50); };
+    } else if (shortname == "scrollupmore") {
+      return function(){ window.scrollBy(0,-500); };
+    } else if (shortname == "scrolldown") {
+      return function(){ window.scrollBy(0,50); };
+    } else if (shortname == "scrolldownmore") {
+      return function(){ window.scrollBy(0,500); };
+
+
+    } else if (shortname == 'back') {
+      return function(){ history.back(); };
+    } else if (shortname == 'forward') {
+      return function(){ history.forward(); };
+
+    } else if (shortname == 'nexttab') {
+      return function(){ chrome.extension.sendRequest({method: "nextTab"}) };
+    } else if (shortname == 'prevtab') {
+      return function(){ chrome.extension.sendRequest({method: "prevTab"}) };
+    }
+
+  }
+});
