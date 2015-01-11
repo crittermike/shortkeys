9a56332c2f9310c17650bfe1b3b6745df4df4b32
Mike Crittenden <mikecrittenden@gmail.com>
2015-01-11 01:08:30 -0500

    basic options page angular working

--- app/scripts/options.js
+++ app/scripts/options.js
@@ -1,3 +1,12 @@
 'use strict';
 
-console.log('\'Allo \'Allo! Option');
+var app = angular.module('ShortkeysOptions', ['ui.bootstrap']);
+app.controller('ShortkeysOptionsCtrl', function($scope) {
+
+
+  $scope.alerts = [{ type: 'warning', msg: 'You MUST reload your browser or tabs after making changes here!'}];
+
+  $scope.closeAlert = function(index) {
+    $scope.alerts.splice(index, 1);
+  };
+});
