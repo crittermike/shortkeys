7a63ab6f4e63c29e352c425fd834ef8f0336c30b
Mike Crittenden <mikecrittenden@gmail.com>
2015-01-11 00:27:16 -0500

    Installing dependencies

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,13 +1,13 @@
 /* global describe, it */
 
 (function () {
-    'use strict';
+  'use strict';
 
-    describe('Give it some context', function () {
-        describe('maybe a bit more context here', function () {
-            it('should run here few assertions', function () {
+  describe('Give it some context', function () {
+    describe('maybe a bit more context here', function () {
+      it('should run here few assertions', function () {
 
-            });
-        });
+      });
     });
+  });
 })();
