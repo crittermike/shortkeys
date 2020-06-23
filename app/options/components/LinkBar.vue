1597550ac04c0aab8cf8af875927cc8918f5f939
Mike Crittenden <mikecrittenden@gmail.com>
2020-06-23 11:18:07 -0400

    Add link bar

---
+++ app/options/components/LinkBar.vue
@@ -0,0 +1,10 @@
+<template>
+    <p style="text-align: center;">
+        <a target="_blank" href="https://salt.bountysource.com/teams/chrome-shortkeys">Donate</a> |
+        <a target="_blank" href="https://chrome.google.com/webstore/detail/shortkeys-custom-keyboard/logpjaacgmcbpdkdchjiaagddngobkck/reviews?hl=en-US&gl=US">Add a Review</a> |
+        <a target="_blank" href="https://github.com/mikecrittenden/shortkeys/wiki/How-To-Use-Shortkeys">Documentation</a> |
+        <a target="_blank" href="https://github.com/mikecrittenden/shortkeys/issues">Support</a> |
+        <a target="_blank" href="https://github.com/mikecrittenden/shortkeys/">Code</a> |
+        <a target="_blank" href="https://chrome.google.com/webstore/detail/shortkeys-custom-keyboard/logpjaacgmcbpdkdchjiaagddngobkck?hl=en-US&gl=US">Webstore</a>
+    </p>
+</template>
--- app/options/options.vue
+++ app/options/options.vue
@@ -1,5 +1,6 @@
 <template>
     <section>
+        <LinkBar />
         <div v-for="key in keys" :key="key.key">
             <ShortcutInput v-model="key.shortcut" />
             <LabelInput v-model="key.label" />
@@ -13,10 +14,12 @@
 import LabelInput from "./components/LabelInput";
 import ShortcutInput from "./components/ShortcutInput";
 import AddButton from "./components/AddButton";
+import LinkBar from "./components/LinkBar";
 
 export default {
     components: {
         ShortcutInput,
+        LinkBar,
         LabelInput,
         AddButton,
     },
