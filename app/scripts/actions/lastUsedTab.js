0f0ebf3953b31dd6919f9f9e0849ec4e8afbfe73
pikou <434708082@qq.com>
2021-03-06 02:16:13 +0800

    fix code style

--- app/scripts/actions/lastUsedTab.js
+++ app/scripts/actions/lastUsedTab.js
@@ -8,38 +8,39 @@ let usedTabInfoQueue = [undefined, undefined]
  * Save used tabs history.
  */
 browser.tabs.onActivated.addListener(function (currentTab) {
-    usedTabInfoQueue = [usedTabInfoQueue[1], currentTab]
+  usedTabInfoQueue = [usedTabInfoQueue[1], currentTab]
 })
 
 /**
- * When close last tab, will unset usedTabInfoQueue[0] 
- * When close current tab, will unset usedTabInfoQueue[1] 
- * When close other tab, will do nothing
- * When close window, will unset usedTabInfoQueue[0] and usedTabInfoQueue[1]
+ * When close last tab, unset usedTabInfoQueue[0] 
+ * When close current tab, unset usedTabInfoQueue[1] 
+ * When close other tab, do nothing
+ * When close window, unset usedTabInfoQueue[0] and usedTabInfoQueue[1]
  */
 browser.tabs.onRemoved.addListener(function (tabId) {
-    // Do not use Array.prototype.filter, it will break queue.
-    // Use Array.prototype.map to keep tab queued in right place.
-    usedTabInfoQueue = usedTabInfoQueue.map(tab => {
-        if (tab && tab.tabId === tabId) {
-            return undefined
-        }
-        return tab
-    })
+  // Do not use Array.prototype.filter, it will break queue.
+  // Use Array.prototype.map to keep tab queued in right place.
+  usedTabInfoQueue = usedTabInfoQueue.map(function (tab) {
+    if (tab && tab.tabId === tabId) {
+      return undefined
+    }
+    return tab
+  })
 })
 
 async function switchToLastUsedTab() {
-    const [lastTab, currentTab] = usedTabInfoQueue
-    if (!lastTab) return
-    if (lastTab.windowId !== currentTab.windowId) {
-        await browser.windows.update(lastTab.windowId, {focused: true})
-    }
-    await browser.tabs.update(lastTab.tabId, {active: true})
-    /**
-     * Call update function will not trigger 'onActivated' event.
-     * So we have to update queue manually.
-     */
-    usedTabInfoQueue = [currentTab, lastTab]
+  const [lastTab, currentTab] = usedTabInfoQueue
+  if (!lastTab) return
+
+  if (lastTab.windowId !== currentTab.windowId) {
+    await browser.windows.update(lastTab.windowId, {focused: true})
+  }
+  await browser.tabs.update(lastTab.tabId, {active: true})
+  /**
+   * Call update function will not trigger 'onActivated' event.
+   * So we have to update queue manually.
+   */
+  usedTabInfoQueue = [currentTab, lastTab]
 }
 
 export default switchToLastUsedTab
